ca80afb967ba5dc4e4286793d306e01520f3426d
Palle <palleklewitz@icloud.com>
2017-02-26 12:58:21 +0100

    Removed Docs

--- make_docs.sh
+++ make_docs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jazzy --clean --author palle-k --author_url https://github.com/palle-k --github_url https://github.com/palle-k/NeuralKit --module NeuralKit --output Docs/ --theme fullwidth
+jazzy --clean --author palle-k --author_url https://github.com/palle-k --github_url https://github.com/palle-k/NeuralKit --module NeuralKit --output docs/ --theme fullwidth
